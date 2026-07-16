a0b980faea4036fc473b83aef1ea501ec29d280a
Rendon Allones <allonesr70671@liceo.edu.ph>
2026-07-16 22:03:37 +0800

    lab-activity-2

---
+++ src/Library/book/book.java
@@ -0,0 +1,34 @@
+package Library.book;
+
+public class book {
+
+    private final String title;
+    private final String author;
+    private boolean isBorrowed;
+
+    public book(String title, String author) {
+        this.title = title;
+        this.author = author;
+        this.isBorrowed = false;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public boolean isBorrowed() {
+        return isBorrowed;
+    }
+
+    public void borrow() {
+        isBorrowed = true;
+    }
+
+    public void returnBook() {
+        isBorrowed = false;
+    }
+}
